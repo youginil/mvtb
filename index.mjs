32a8145633e553957914b789a4f38637b4c829cf
youginil <youginil@outlook.com>
2023-04-28 15:00:54 +0800

    refactoring

--- index.mjs
+++ index.mjs
@@ -5,8 +5,8 @@ import chalk from 'chalk';
 import { execSync } from 'child_process';
 import fs from 'fs';
 import path from 'path';
-import TextToSVG from 'text-to-svg';
 import cliProgress from 'cli-progress';
+import { createCanvas } from 'canvas';
 
 function error(msg) {
     console.log(chalk.red(msg));
@@ -22,142 +22,87 @@ function sec2str(s) {
     return r;
 }
 
-program
-    .name('mvtb')
-    .description('MoVie ThumBnail generator')
-    .option('-f, --file <string>', 'movie file')
-    .option('-d, --directory <string>', 'movie directory')
-    .option(
-        '-e, --ext <string>',
-        'file extensions, split by |',
-        'avi|wmv|mp4|mov|rmvb|mkv|m4v|flv|3gp'
-    )
-    .option('-R, --row <number>', 'thumb rows', 4)
-    .option('-C, --column <number>', 'thumb columns', 4)
-    .option('-W, --width <number>', 'thumb width', 250);
-
-program.parse();
-
-const opts = program.opts();
-opts.row = Math.ceil(opts.row);
-if (opts.row <= 0) {
-    error('Invalid row');
-}
-opts.column = Math.ceil(opts.column);
-if (opts.column <= 0) {
-    error('Invalid column');
-}
-const grids = opts.row * opts.column;
-
-if (opts.file) {
-    const bar = new cliProgress.SingleBar({
-        format: ' {bar} | {value}/{total}',
-    });
-    bar.start(grids, 0);
-    await make(opts.file, bar);
-    bar.stop();
-} else if (opts.directory) {
-    const reg = new RegExp(`\.(${opts.ext})$`, 'i');
-    const files = fs
-        .readdirSync(opts.directory)
-        .filter((item) => reg.test(item));
-    if (files.length > 0) {
-        const bars = new cliProgress.MultiBar({
-            format: ' {bar} | {filename} | {value}/{total}',
-        });
-        const b1 = bars.create(files.length, 0, { filename: 'ALL' });
-        const b2 = bars.create(grids, 0, { filename: '' });
-        for (let i = 0; i < files.length; i++) {
-            const file = path.resolve(opts.directory, files[i]);
-            await make(file, b2, bars);
-            b1.increment();
-            bars.update();
-        }
-        bars.stop();
-    }
-} else {
-    error('Please specify file or directory');
+function makeTextImage(text, width, height, fontSize) {
+    const canvas = createCanvas(width, height);
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+    ctx.font = fontSize + 'px Arial';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(text, 0, Math.ceil(height / 2));
+    return canvas.toBuffer('image/png');
 }
 
-async function make(file, bar, bars) {
-    bar.update(0, { filename: path.basename(file) });
-    bars && bars.update();
+async function make(file, ignoreOutput) {
     const picDir = fs.mkdtempSync('mvtb');
     try {
         const duration = execSync(
             `ffprobe -i "${file}" -show_entries format=duration -v quiet -of csv="p=0"`
         ).toString();
+        const fps = grids / duration;
         const picNum = opts.row * opts.column;
-        const space = duration / picNum;
-        const pics = [];
-        let t = 0;
         const picNumLen = `${picNum}`.length;
-        for (let i = 1; i <= picNum; i++) {
-            const pic = path.resolve(
-                picDir,
-                ('' + i).padStart(picNumLen, '0') + '.png'
+        const dest = path.resolve(picDir, '%0' + picNumLen + 'd.png');
+        const cmd = `ffmpeg -i "${file}" -vf "fps=${fps},scale=${opts.width}:-1" ${dest}`;
+        execSync(cmd, { stdio: ignoreOutput ? 'ignore' : 'inherit' });
+        const pics = [];
+        for (let i = 0; i < picNum; i++) {
+            pics.push(
+                path.resolve(
+                    picDir,
+                    (i + 1).toString().padStart(picNumLen, '0') + '.png'
+                )
             );
-            const ts = sec2str(t);
-            execSync(
-                `ffmpeg -ss ${ts} -i "${file}" -y -f image2 -vframes 1 "${pic}"`,
-                {
-                    stdio: 'ignore',
-                }
-            );
-            t += space;
-            pics.push({
-                pic,
-                ts,
-            });
-            bar.increment();
-            bars && bars.update();
         }
         const dir = path.dirname(file);
-        const meta = await sharp(pics[0].pic).metadata();
+        const meta = await sharp(pics[0]).metadata();
         const w = opts.width;
         const h = Math.ceil((w / meta.width) * meta.height);
-        const r = opts.row;
-        const c = opts.column;
+        const outWidth = w * columns;
+        const textHeight = 50;
+        const fontSize = 36;
+        const headerHeight = textHeight * 2;
+        const overlay = [
+            {
+                input: makeTextImage(
+                    'Filename: ' + path.basename(file),
+                    outWidth,
+                    textHeight,
+                    fontSize
+                ),
+                top: 0,
+                left: 0,
+            },
+            {
+                input: makeTextImage(
+                    'Duration: ' + sec2str(duration),
+                    outWidth,
+                    textHeight,
+                    fontSize
+                ),
+                top: textHeight,
+                left: 0,
+            },
+        ];
         const sh = sharp({
             create: {
-                width: w * c,
-                height: h * r,
+                width: outWidth,
+                height: h * rows + headerHeight,
                 channels: 4,
                 background: '#000000',
             },
         });
-        const tts = TextToSVG.loadSync();
-        const overlay = [];
         let left = 0;
-        let top = 0;
+        let top = headerHeight;
         for (let i = 0; i < pics.length; i++) {
-            const svg = tts.getSVG(pics[i].ts, {
-                x: 0,
-                y: 0,
-                fontSize: 20,
-                anchor: 'top',
-                attributes: {
-                    stroke: 'white',
-                    fill: 'white',
-                },
-            });
-            const txt = sharp(Buffer.from(svg));
-            const txtMeta = await txt.metadata();
-            if (txtMeta.width > w) {
-                txt.resize({ width: w });
-            }
-            const p = await sharp(pics[i].pic)
+            const p = await sharp(pics[i])
                 .resize({ width: w, height: h })
-                .composite([
-                    { input: await txt.toBuffer(), gravity: 'southeast' },
-                ])
                 .toBuffer();
             overlay.push({
                 input: p,
                 top,
                 left,
             });
-            left = (i + 1) % c === 0 ? 0 : left + w;
+            left = (i + 1) % columns === 0 ? 0 : left + w;
             if (left === 0) {
                 top += h;
             }
@@ -180,5 +125,57 @@ async function make(file, bar, bars) {
             }
         });
     }
+}
+
+program
+    .name('mvtb')
+    .description('MoVie ThumBnail generator')
+    .option('-f, --file <string>', 'movie file')
+    .option('-d, --directory <string>', 'movie directory')
+    .option(
+        '-e, --ext <string>',
+        'file extensions, split by |',
+        'avi|wmv|mp4|mov|rmvb|mkv|m4v|flv|3gp'
+    )
+    .option('-R, --row <number>', 'thumb rows', 4)
+    .option('-C, --column <number>', 'thumb columns', 4)
+    .option('-W, --width <number>', 'thumb width', 250);
+
+program.parse();
+
+const opts = program.opts();
+const rows = Math.ceil(opts.row);
+if (rows <= 0) {
+    error('Invalid row');
+}
+const columns = Math.ceil(opts.column);
+if (columns <= 0) {
+    error('Invalid column');
+}
+const grids = rows * columns;
+
+if (opts.file) {
+    await make(opts.file, false);
+} else if (opts.directory) {
+    console.log('\n');
+    const reg = new RegExp(`\.(${opts.ext})$`, 'i');
+    const files = fs
+        .readdirSync(opts.directory)
+        .filter((item) => !item.startsWith('.') && reg.test(item));
+    if (files.length > 0) {
+        const bar = new cliProgress.SingleBar({
+            format: ' {bar} | {filename} | {value}/{total}',
+        });
+        bar.start(files.length, 0);
+        for (let i = 0; i < files.length; i++) {
+            const file = path.resolve(opts.directory, files[i]);
+            bar.update(i + 1, { filename: files[i] });
+            bar.render();
+            await make(file, true);
+        }
+        bar.stop();
+    }
+} else {
+    error('Please specify file or directory');
 }
 
